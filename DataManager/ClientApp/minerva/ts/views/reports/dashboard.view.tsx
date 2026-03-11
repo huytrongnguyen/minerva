69240f8c5fb7abaa6cf43e0e826dc2b60a9c7ffa
huytrongnguyen <huynguyen8505@gmail.com>
2026-03-11 16:45:35 +0700

    - Add `Create Report` dialog

--- DataManager/ClientApp/minerva/ts/views/reports/dashboard.view.tsx
+++ DataManager/ClientApp/minerva/ts/views/reports/dashboard.view.tsx
@@ -3,12 +3,16 @@ import { useParams } from 'react-router-dom';
 import { ProductDashboard, ReportDefinition, UpdateProductDashboardModel } from 'minerva/core';
 import { ProductLayout, ProductSelector } from 'minerva/components';
 import { ReportComponent } from './report.component';
+import { useDialog } from 'rosie/components';
+import { Rosie } from 'rosie/core';
+import { ReportCreationDialog } from './report-creation.component';
 
 export function DashboardView() {
   const params = useParams(),
         [productId, setProductId] = useState(''),
         [dashboardId, setDashboardId] = useState(''),
         [dashboardName, setDashboardName] = useState(''),
+        reportCreationDialog = useDialog('#report-creation-dialog'),
         [layout, setLayout] = useState<ReportDefinition[][]>([]);
 
   useEffect(() => {
@@ -38,16 +42,25 @@ export function DashboardView() {
     ProductDashboard.loadData(dashboard);
   }
 
+  function onCreateReportSuccess() {
+    Rosie.hideModal('#report-creation-dialog');
+  }
+
   return <ProductLayout>
     <ProductSelector navPath='/dashboard'>
       <li className="breadcrumb-item active">{dashboardName ?? 'Dashboard'}</li>
-      <div className="dropdown ms-auto">
-        <button className="btn btn-sm btn-outline-secondary dropdown-toggle hide-indicator" data-bs-toggle="dropdown" data-bs-auto-close="true">
-          <span className="fa fa-ellipsis" />
-        </button>
-        <div className="dropdown-menu">
-          <button className="dropdown-item" onClick={() => saveDashboardReports()}>Save Dashboard</button>
+      <div className="d-flex ms-auto">
+        <div className="dropdown me-1">
+          <button className="btn btn-sm btn-outline-secondary dropdown-toggle hide-indicator" data-bs-toggle="dropdown" data-bs-auto-close="true">
+            <span className="fa fa-ellipsis" />
+          </button>
+          <div className="dropdown-menu">
+            <button className="dropdown-item" onClick={() => saveDashboardReports()}>Save Dashboard</button>
+          </div>
         </div>
+        <button className="btn btn-sm btn-primary" onClick={() => reportCreationDialog.show()}>
+          <span className="fa fa-plus" />
+        </button>
       </div>
     </ProductSelector>
     <main className="fullscreen">
@@ -60,6 +73,8 @@ export function DashboardView() {
         })}
       </div>}
     </main>
+    {reportCreationDialog.isShown && <ReportCreationDialog productId={productId} dashboardId={dashboardId}
+            onCreateSuccess={onCreateReportSuccess} />}
   </ProductLayout>
 }
 
